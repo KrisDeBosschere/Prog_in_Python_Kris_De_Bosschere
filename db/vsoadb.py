6fba51b210b6f0adb09ea924ec46e3f3dec11e50
Kris De Bosschere <kris.debosschere@student.vives.be>
2023-11-02 12:53:53 +0100

    opbouw structuur van het project

--- db/vsoadb.py
+++ db/vsoadb.py
@@ -1,5 +1,21 @@
 import Prog_in_Python_Kris_De_Bosschere.domain.vsoa
+import Prog_in_Python_Kris_De_Bosschere.domain.medewerker
+import Prog_in_Python_Kris_De_Bosschere.settings
+
 
 def get_all_dossiers():
 	# SQL statements
 	# lijst van dossierobjecten teruggeven
+
+def get_one_dossier():
+	# 1 specifiek dossier per naam
+
+def get_medewerkers():
+	# alle medewerkers opvragen
+
+def set_medewerkers():
+	# medewerker toevoegen
+
+def set_dossier():
+	# dossier toevoegen
+
---
+++ domain/medewerker.py
@@ -0,0 +1,1 @@
+#klasse medewerker definiëren
