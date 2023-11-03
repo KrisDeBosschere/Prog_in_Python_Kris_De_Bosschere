248bd3a81c7fe12794272a5881c7511e340d6452
Kris De Bosschere <kris.debosschere@student.vives.be>
2023-11-03 08:54:00 +0100

    Uitbouwen van alle logica voor user interactie en database acties

--- db/vsoadb.py
+++ db/vsoadb.py
@@ -7,16 +7,46 @@ import settings
 
 
 def get_all_dossiers():
-	with sqlite3.connect("project.db") as dbconnectie:
-		resultaat = dbconnectie.execute("SELECT id, lid, type, verantwoordelijke FROM dossier")
-		print(resultaat.fetchall())
+    try:
+        with sqlite3.connect("../project.db") as dbconnectie:
+            mijncursor = dbconnectie.cursor()
+            mijncursor.execute("SELECT id, lid, type, verantwoordelijke FROM dossier")
+            resultaat = mijncursor.fetchall()
+            return resultaat
+    except sqlite3.Error as e:
+        print("Fout bij het ophalen van dossiers:", str(e))
+        return None
 
-# def get_medewerkers():
-	# alle medewerkers opvragen
+def get_all_medewerkers():
+    try:
+        with sqlite3.connect("../project.db") as dbconnectie:
+            mijncursor = dbconnectie.cursor()
+            mijncursor.execute("SELECT id, naam FROM medewerker")
+            resultaat = mijncursor.fetchall()
+            return resultaat
+    except sqlite3.Error as e:
+        print("Fout bij het ophalen van medewerkers:", str(e))
+        return None
 
-# def set_medewerkers():
-	# medewerker toevoegen
+def set_medewerker(medewerker):
+    try:
+        # Medewerker toevoegen
+        with sqlite3.connect("../project.db") as dbconnectie:
+            mijncursor = dbconnectie.cursor()
+            query = "INSERT INTO medewerker (naam) VALUES (?)"
+            mijncursor.execute(query, (medewerker,))
+            dbconnectie.commit()
+    except sqlite3.Error as e:
+        print("Fout bij het toevoegen van medewerker:", str(e))
 
-# def set_dossier():
-	# dossier toevoegen
+def set_dossier(lid, dossier_type, verantwoordelijke):
+    try:
+        # Dossier toevoegen
+        with sqlite3.connect("../project.db") as dbconnectie:
+            mijncursor = dbconnectie.cursor()
+            query = "INSERT INTO dossier (lid, type, verantwoordelijke) VALUES (?, ?, ?)"
+            mijncursor.execute(query, (lid, dossier_type, verantwoordelijke))
+            dbconnectie.commit()
+    except sqlite3.Error as e:
+        print("Fout bij het toevoegen van dossier:", str(e))
 
--- user/dossierconsult.py
+++ user/dossierconsult.py
@@ -1,21 +1,53 @@
 import sys
 import os
+import sqlite3
 
-# Add the path to the master folder to sys.path
+# Voeg het pad naar de hoofdmap toe aan sys.path
 master_folder = r"C:\Users\krisd\pythonscripts\Prog_in_Python_Kris_De_Bosschere"
 sys.path.append(master_folder)
 
-# import modules from other directories
+# Importeer modules uit andere mappen
 from db import vsoadb
 from domain import vsoa
 from domain import medewerker
 import settings
-import sqlite3
 
+def main():
+    keuze = input("Wat zou je willen doen? \n Alle dossiers opvragen, typ 1 \n Alle medewerkers opvragen, typ 2 \n Een medewerker toevoegen, typ 3 \n Een dossier toevoegen, typ 4 \n Een CSV-bestand krijgen van alle dossiers, typ 5 \n ")
 
-keuze = input("Wat zou je willen doen? \n Alle dossiers opvragen, typ 1 \n Alle medewerkers opvragen, typ 2 \n Een medewerker toevoegen, typ 3 \n Een dossier toevoegen, typ 4 \n Een csv bestand krijgen van alle dossiers, typ 5 \n ")
-if keuze not in ('1','2','3','4','5'):
-	print("Je gaf een verkeerde input!")
-else :
-	print(f"Je koos voor {keuze}")
+    if keuze not in ('1', '2', '3', '4', '5'):
+        print("Je gaf een verkeerde input!")
+    else:
+        print(f"Je koos voor {keuze}")
 
+    if keuze == '1':
+        dossiers = vsoadb.get_all_dossiers()
+        if dossiers:
+            for dossier in dossiers:
+                print(dossier)
+        else:
+            print("Geen dossiers gevonden.")
+    elif keuze == '2':
+        medewerkers = vsoadb.get_all_medewerkers()
+        if medewerkers:
+            for medewerker in medewerkers:
+                print(medewerker)
+        else:
+            print("Geen medewerkers gevonden.")
+    elif keuze == '3':
+        naam = input("Voer de naam van de medewerker in: ")
+        vsoadb.set_medewerker(naam)
+        print("Medewerker toegevoegd.")
+    elif keuze == '4':
+        lid = input("Voer het lid in: ")
+        dossier_type = input("Voer het type in: ")
+        verantwoordelijke = input("Voer de verantwoordelijke in: ")
+        vsoadb.set_dossier(lid, dossier_type, verantwoordelijke)
+        print("Dossier toegevoegd.")
+    elif keuze == '5':
+        pass  # Voeg hier de logica toe om een CSV-bestand van alle dossiers te genereren
+    else:
+        pass
+
+if __name__ == "__main__":
+    main()
